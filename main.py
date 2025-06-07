9da619a952191e1d8702b16de3078faca5f0df4f
Audemar-Firebrand <19tristan.martin@gmail.com>
2025-06-06 20:21:53 -0600

    Word count added to bookbot

--- main.py
+++ main.py
@@ -2,4 +2,12 @@ def get_book_text():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
         print(file_contents)
-call: get_book_text()
+
+def book_word_count():
+    with open("books/frankenstein.txt") as f:
+       file_contents = f.read()
+       words = file_contents.split()
+       word_count = len(words)
+    print(f"{word_count} words found in the document")
+
+call: book_word_count()
